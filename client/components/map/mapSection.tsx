44a413797b6428080f0cc9057a61f797cd36d523
Johannes Brodwall <johannes@brodwall.com>
2023-11-11 19:31:41 +0100

    display OSM map

--- client/components/map/mapSection.tsx
+++ client/components/map/mapSection.tsx
@@ -1,8 +1,21 @@
 import * as React from "react";
-import { MutableRefObject, useRef } from "react";
+import { MutableRefObject, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
 
 export function MapSection() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-
-  return <main ref={mapRef}>I'm a great map</main>;
+  useEffect(() => {
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: mapRef.current,
+      view: new View({ center: [0, 0], zoom: 4 }),
+    });
+  }, []);
+  return <main ref={mapRef}></main>;
 }
