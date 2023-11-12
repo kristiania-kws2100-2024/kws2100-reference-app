d1622dc68867a8ad0f356f26620304548d5fa5db
Johannes Brodwall <johannes@brodwall.com>
2023-11-12 22:05:22 +0100

    extract useKommuneLayer

--- client/components/application/appMainSection.tsx
+++ client/components/application/appMainSection.tsx
@@ -3,54 +3,16 @@ import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
 import { MapSection } from "../map/mapSection";
 import { RightSidebar } from "./rightSidebar";
 import { LeftSidebar } from "./leftSidebar";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import { Fill, Stroke, Style, Text } from "ol/style";
 import { Map, View } from "ol";
-import { sortBy } from "../../lib/sortBy";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
-import { FeatureLike } from "ol/Feature";
-import { useFeatureSelector } from "../map/useFeatureSelector";
-import { useHoveredFeature } from "../map/useHoveredFeature";
-import {
-  kommuneAsFeature,
-  KommuneFeatureCollectionDto,
-  KommunePropertiesDto,
-} from "../../lib/norway";
+import { KommunePropertiesDto } from "../../lib/norway";
+import { useKommuneLayer } from "./useKommuneLayer";
 
 useGeographic();
-
-const kommuneStyle = new Style({
-  stroke: new Stroke({ color: "blue", width: 0.2 }),
-});
-const kommuneStyleHighlight = {
-  stroke: new Stroke({ color: "blue", width: 2 }),
-  fill: new Fill({ color: [0xff, 0xff, 0xff, 0.1] }),
-};
-const kommuneStyleTextHighlight = {
-  font: "18px Calibri,sans-serif,bold",
-  fill: new Fill({ color: "black" }),
-  stroke: new Stroke({ color: "white", width: 1 }),
-};
-
-function kommuneStyleFn(feature: FeatureLike): Style {
-  const properties = feature.getProperties() as KommunePropertiesDto & {
-    selected?: boolean;
-  };
-  return !properties.selected
-    ? kommuneStyle
-    : new Style({
-        ...kommuneStyleHighlight,
-        text: new Text({
-          ...kommuneStyleTextHighlight,
-          text: properties.navn.find((n) => n.sprak === "nor")?.navn,
-        }),
-      });
-}
 
 export function AppMainSection({
   focusKommune,
@@ -65,7 +27,6 @@ export function AppMainSection({
     });
   }, []);
 
-  const [kommuneList, setKommuneList] = useState<KommunePropertiesDto[]>([]);
   const [selectedKommune, setSelectedKommune] =
     useState<KommunePropertiesDto>();
 
@@ -74,39 +35,15 @@ export function AppMainSection({
     [],
   );
 
-  const kommuneSource = useMemo(() => new VectorSource(), []);
-  const kommuneLayer = useMemo(
-    () => new VectorLayer({ source: kommuneSource, style: kommuneStyleFn }),
-    [kommuneSource],
-  );
-  useFeatureSelector(kommuneSource, selectedKommune?.kommunenummer);
-  const hoverKommuneFeature = useHoveredFeature(map, kommuneSource);
-  useEffect(() => {
-    setFocusKommune(
-      hoverKommuneFeature?.getProperties() as KommunePropertiesDto,
-    );
-  }, [hoverKommuneFeature]);
+  const {
+    kommuneList,
+    layer: kommuneLayer,
+    hoveredKommune,
+  } = useKommuneLayer(map, "/geojson/kommuner.geojson", selectedKommune);
+  useEffect(() => setFocusKommune(hoveredKommune), [hoveredKommune]);
 
   const layers = useMemo(() => [backgroundLayer, kommuneLayer], [kommuneLayer]);
   useEffect(() => map.setLayers(layers), [layers]);
-
-  async function loadKommuneList() {
-    setFocusKommune(undefined);
-    const res = await fetch("/geojson/kommuner.geojson");
-    const kommuneFeatures = (await res.json()) as KommuneFeatureCollectionDto;
-    setKommuneList(
-      kommuneFeatures.features
-        .map((f) => f.properties)
-        .sort(sortBy((p) => p.navn.find((n) => n.sprak === "nor")?.navn!)),
-    );
-    for (const featureDto of kommuneFeatures.features) {
-      kommuneSource.addFeature(kommuneAsFeature(featureDto));
-    }
-  }
-
-  useEffect(() => {
-    loadKommuneList().then();
-  }, []);
 
   return (
     <section id={"content"}>
---
+++ client/components/application/useKommuneLayer.tsx
@@ -0,0 +1,81 @@
+import { Fill, Stroke, Style, Text } from "ol/style";
+import { FeatureLike } from "ol/Feature";
+import {
+  kommuneAsFeature,
+  KommuneFeatureCollectionDto,
+  KommunePropertiesDto,
+} from "../../lib/norway";
+import { Map } from "ol";
+import { useEffect, useMemo, useState } from "react";
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import { sortBy } from "../../lib/sortBy";
+import { useFeatureSelector } from "../map/useFeatureSelector";
+import { useHoveredFeature } from "../map/useHoveredFeature";
+
+const kommuneStyle = new Style({
+  stroke: new Stroke({ color: "blue", width: 0.2 }),
+});
+const kommuneStyleHighlight = {
+  stroke: new Stroke({ color: "blue", width: 2 }),
+  fill: new Fill({ color: [0xff, 0xff, 0xff, 0.1] }),
+};
+const kommuneStyleTextHighlight = {
+  font: "18px Calibri,sans-serif,bold",
+  fill: new Fill({ color: "black" }),
+  stroke: new Stroke({ color: "white", width: 1 }),
+};
+
+function kommuneStyleFn(feature: FeatureLike): Style {
+  const properties = feature.getProperties() as KommunePropertiesDto & {
+    selected?: boolean;
+  };
+  return !properties.selected
+    ? kommuneStyle
+    : new Style({
+        ...kommuneStyleHighlight,
+        text: new Text({
+          ...kommuneStyleTextHighlight,
+          text: properties.navn.find((n) => n.sprak === "nor")?.navn,
+        }),
+      });
+}
+
+export function useKommuneLayer(
+  map: Map,
+  path: string,
+  selected?: KommunePropertiesDto,
+) {
+  const [kommuneList, setKommuneList] = useState<KommunePropertiesDto[]>([]);
+  const source = useMemo(() => new VectorSource(), []);
+  const layer = useMemo(
+    () => new VectorLayer({ source, style: kommuneStyleFn }),
+    [source],
+  );
+
+  async function loadKommuneList() {
+    const res = await fetch(path);
+    const kommuneFeatures = (await res.json()) as KommuneFeatureCollectionDto;
+    setKommuneList(
+      kommuneFeatures.features
+        .map((f) => f.properties)
+        .sort(sortBy((p) => p.navn.find((n) => n.sprak === "nor")?.navn!)),
+    );
+    for (const featureDto of kommuneFeatures.features) {
+      source.addFeature(kommuneAsFeature(featureDto));
+    }
+  }
+
+  useFeatureSelector(source, selected?.kommunenummer);
+  const hoveredFeature = useHoveredFeature(map, source);
+  const hoveredKommune = useMemo(
+    () => hoveredFeature?.getProperties() as KommunePropertiesDto,
+    [hoveredFeature],
+  );
+
+  useEffect(() => {
+    loadKommuneList().then();
+  }, []);
+
+  return { kommuneList, layer, hoveredKommune };
+}
