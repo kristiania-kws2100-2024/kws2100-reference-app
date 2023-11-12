5915d04f9f496181973369f03f370bba0ac990ae
Johannes Brodwall <johannes@brodwall.com>
2023-11-12 20:25:41 +0100

    pull up OL map object

--- client/components/application/appMainSection.tsx
+++ client/components/application/appMainSection.tsx
@@ -6,13 +6,20 @@ import { LeftSidebar } from "./leftSidebar";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { Stroke, Style } from "ol/style";
-import { Feature } from "ol";
+import { Feature, Map, View } from "ol";
 import { Polygon } from "ol/geom";
 import {
   KommuneFeatureCollectionDto,
   KommunePropertiesDto,
 } from "../../../lib/norway";
 import { sortBy } from "../../lib/sortBy";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+
+import "ol/ol.css";
+
+useGeographic();
 
 const kommuneStyle = new Style({
   stroke: new Stroke({ color: "blue", width: 0.2 }),
@@ -20,11 +27,25 @@ const kommuneStyle = new Style({
 
 export function AppMainSection() {
   const [kommuneList, setKommuneList] = useState<KommunePropertiesDto[]>([]);
+
+  const backgroundLayer = useMemo(
+    () => new TileLayer({ source: new OSM() }),
+    [],
+  );
+
   const kommuneSource = useMemo(() => new VectorSource(), []);
   const kommuneLayer = useMemo(
     () => new VectorLayer({ source: kommuneSource, style: kommuneStyle }),
     [kommuneSource],
   );
+
+  const layers = useMemo(() => [backgroundLayer, kommuneLayer], [kommuneLayer]);
+  const map = useMemo(() => {
+    return new Map({
+      view: new View({ center: [11, 60], zoom: 10 }),
+    });
+  }, []);
+  useEffect(() => map.setLayers(layers), [layers]);
 
   async function loadKommuneList() {
     const res = await fetch("/geojson/kommuner.geojson");
@@ -34,15 +55,12 @@ export function AppMainSection() {
         .map((f) => f.properties)
         .sort(sortBy((p) => p.navn.find((n) => n.sprak === "nor")?.navn!)),
     );
-    for (const {
-      geometry: { coordinates },
-      properties,
-    } of kommuneFeatures.features) {
+    for (const featureDto of kommuneFeatures.features) {
       const feature = new Feature({
-        ...properties,
-        geometry: new Polygon(coordinates),
+        ...featureDto.properties,
+        geometry: new Polygon(featureDto.geometry.coordinates),
       });
-      feature.setId(properties.kommunenummer);
+      feature.setId(featureDto.properties.kommunenummer);
       kommuneSource.addFeature(feature);
     }
   }
@@ -54,7 +72,7 @@ export function AppMainSection() {
   return (
     <section id={"content"}>
       <LeftSidebar />
-      <MapSection kommuneLayer={kommuneLayer} />
+      <MapSection map={map} />
       <RightSidebar kommuneList={kommuneList} />
     </section>
   );
--- client/components/map/mapSection.tsx
+++ client/components/map/mapSection.tsx
@@ -1,33 +1,9 @@
 import * as React from "react";
 import { MutableRefObject, useEffect, useRef } from "react";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
-import { useGeographic } from "ol/proj";
+import { Map } from "ol";
 
-import "ol/ol.css";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-
-useGeographic();
-
-export function MapSection({
-  kommuneLayer,
-}: {
-  kommuneLayer: VectorLayer<VectorSource>;
-}) {
+export function MapSection({ map }: { map: Map }) {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-  useEffect(() => {
-    new Map({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-        kommuneLayer,
-      ],
-      target: mapRef.current,
-      view: new View({ center: [11, 60], zoom: 10 }),
-    });
-  }, []);
+  useEffect(() => map.setTarget(mapRef.current), []);
   return <main ref={mapRef}></main>;
 }
