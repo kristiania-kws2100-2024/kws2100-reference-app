90a254c5c0f68614c60858e7af81e76243cf2bad
Johannes Brodwall <johannes@brodwall.com>
2023-11-13 23:17:15 +0100

    Import norwegian roads from XML (dirty, dirty code!)

--- data/process.ts
+++ data/process.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as sax from "sax";
-import pg from "pg";
+import * as pg from "pg";
 
 const pool = new pg.Pool({
   user: "postgres",
@@ -9,12 +9,15 @@ const pool = new pg.Pool({
 });
 
 const ROAD_FIELDS = [
+  "id",
   "lokalId",
   "lineString",
   "kommunenummer",
   "typeVeg",
   "adressekode",
   "adressenavn",
+  "vegnummer",
+  "startposisjon",
   "sluttposisjon",
   "fraMeter",
   "tilMeter",
@@ -29,13 +32,72 @@ type Road = Record<RoadFieldType, string | undefined>;
 const FILES =
   "../../../../../Downloads/Samferdsel_0000_Norge_5973_Elveg2-0_GML";
 
+function chunks<T>(arr: T[], size: number): T[][] {
+  let chunks = [],
+    i = 0,
+    n = arr.length;
+
+  while (i < n) {
+    chunks.push(arr.slice(i, (i += size)));
+  }
+
+  return chunks;
+}
+
+async function insertRoads(rows: Road[]) {
+  const tableName = "norway_roads";
+  const columns = ROAD_FIELDS;
+  if (rows.length == 0) {
+    return;
+  }
+  console.log("bulk inserting", rows.length, tableName);
+  const paramLines: string[] = [];
+  const values: unknown[] = [];
+  for (let i = 0; i < rows.length; i++) {
+    const offset = i * (columns.length + 2);
+    paramLines.push(
+      "(" +
+        columns.map((_, i) => `$${i + offset + 1}`).join(", ") +
+        ", st_setsrid($" +
+        (offset + columns.length + 1) +
+        "::geometry, $" +
+        (offset + columns.length + 2) +
+        "))",
+    );
+    for (const field of columns) {
+      values.push(rows[i][field]);
+    }
+    values.push(
+      "linestring z(" +
+        chunks(rows[i].lineString!.split(" "), 3)
+          .map((chunk) => chunk.join(" "))
+          .join(", ") +
+        ")",
+    );
+    values.push(
+      parseInt(
+        rows[i].srsName!.substring(rows[i].srsName!.lastIndexOf("/") + 1),
+      ),
+    );
+  }
+  const text = `insert into ${tableName}
+    (${columns.join(", ")}, geom)
+values
+${paramLines.join(",\n")}`;
+  await pool.query({ text, values });
+}
+
 async function process(file: string) {
-  var stream = fs.createReadStream(path.join(FILES, file));
+  console.log("importing", file);
+
+  await pool.query("BEGIN");
+
+  const stream = fs.createReadStream(path.join(FILES, file));
   const parser = sax.createStream(true, {
     normalize: false,
   });
 
-  const roads: Road[] = [];
+  let roads: Road[] = [];
 
   const stack: string[] = [];
   let road: Partial<Road> | undefined = undefined;
@@ -45,7 +107,9 @@ async function process(file: string) {
     nodeText = "";
     stack.push(tag.name);
     if (tag.name === "Veglenke") {
-      road = {};
+      road = {
+        id: tag.attributes["gml:id"] as string,
+      };
     }
     if (tag.name === "gml:LineString") {
       srsName = tag.attributes["srsName"] as string;
@@ -55,11 +119,15 @@ async function process(file: string) {
   parser.on("text", (text) => {
     nodeText += text;
   });
-  parser.on("closetag", (tagName) => {
+  parser.on("closetag", async (tagName) => {
     stack.pop();
     if (tagName === "Veglenke") {
       roads.push(road as Road);
       road = undefined;
+      if (roads.length === 1000) {
+        await insertRoads(roads);
+        roads = [];
+      }
     }
     if (road) {
       if (tagName === "lokalId") {
@@ -75,48 +143,78 @@ async function process(file: string) {
         road.adressekode = nodeText;
       } else if (tagName === "adressenavn") {
         road.adressenavn = nodeText;
+      } else if (tagName === "vegnummer") {
+        road.vegnummer = nodeText;
       } else if (tagName === "fraMeter") {
         road.fraMeter = nodeText;
       } else if (tagName === "tilMeter") {
         road.tilMeter = nodeText;
       } else if (tagName === "strekningNummer") {
         road.strekningNummer = nodeText;
-      } else if (tagName === "delStrekningNummer") {
+      } else if (tagName === "delstrekningNummer") {
         road.delStrekningNummer = nodeText;
+      } else if (tagName === "startposisjon") {
+        road.startposisjon = nodeText;
       } else if (tagName === "sluttposisjon") {
         road.sluttposisjon = nodeText;
       }
     }
   });
 
-  parser.on("end", async () => {
-    const batchSize = 100;
-    for (let i = 0; i < roads.length; i += batchSize) {
-      const parameterLines: string[] = [];
-      const values: unknown[] = [];
-      for (let j = 0; j < batchSize && j + i < roads.length; j++) {
-        const offset = j * ROAD_FIELDS.length;
-        parameterLines.push(
-          "(" +
-            ROAD_FIELDS.map((_, index) => `$${index + offset + 1}`).join(", ") +
-            ")",
-        );
-        for (const field of ROAD_FIELDS) {
-          values.push(roads[i + j][field]);
-        }
-      }
-      const text = `insert into norway_roads (${ROAD_FIELDS.join(", ")})
-      values
-      ${parameterLines.join(",\n")}
-      `;
-      await pool.query({ text, values });
-    }
+  const promise = new Promise((resolve) => {
+    parser.on("end", async () => {
+      await insertRoads(roads);
+      await pool.query("COMMIT");
+      resolve(null);
+    });
   });
 
   stream.pipe(parser);
+  return promise;
 }
 
-const files = fs.readdirSync(FILES);
-for (const file of files) {
-  process(file).then();
+async function readFiles() {
+  const files = fs.readdirSync(FILES);
+  const result = (
+    await pool.query(
+      "select kommunenummer, count(*) from norway_roads group by kommunenummer",
+    )
+  ).rows;
+  const alreadyImportedFiles = result
+    .map((r) => r["kommunenummer"])
+    .flatMap((k) => files.filter((f) => f.startsWith(k)));
+  console.log("Skipping", alreadyImportedFiles);
+  for (const file of files) {
+    if (!alreadyImportedFiles.includes(file)) {
+      await process(file);
+    }
+  }
 }
+
+const createTable = `
+create table norway_roads
+(
+    id                 varchar(20),
+    kommunenummer      varchar(4),
+    lokalid            integer,
+    geom               geometry,
+    linestring         text,
+    typeveg            varchar,
+    adressekode        varchar,
+    adressenavn        varchar,
+    vegnummer          integer,
+    startposisjon      double precision,
+    sluttposisjon      double precision,
+    frameter           numeric,
+    tilmeter           numeric,
+    strekningnummer    integer,
+    delstrekningnummer integer,
+    srsname            varchar,
+    primary key (id, kommunenummer)
+);
+  
+`;
+
+readFiles().then();
+//process("5442Elveg2.0.gml").then();
+//process("1111Elveg2.0.gml").then();
