2cd2b86944b7ff16ca01d85ab8e43db4aba9bef0
Johannes Brodwall <johannes@brodwall.com>
2024-05-05 20:55:26 +0200

    Show VectorTileSource

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -6,6 +6,9 @@ import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 
 import "ol/ol.css";
+import VectorTileLayer from "ol/layer/VectorTile";
+import VectorTileSource from "ol/source/VectorTile";
+import { MVT } from "ol/format";
 
 useGeographic();
 const map = new Map({
@@ -14,11 +17,21 @@ const map = new Map({
 
 const osmLayer = new TileLayer({ source: new OSM() });
 
+const vectorTileUrl =
+  "https://ahocevar.com/geoserver/gwc/service/tms/1.0.0/ne:ne_10m_admin_0_countries@EPSG%3A900913@pbf/{z}/{x}/{-y}.pbf";
+const vectorTileLayer = new VectorTileLayer({
+  source: new VectorTileSource({
+    format: new MVT(),
+    url: vectorTileUrl,
+    maxZoom: 14,
+  }),
+});
+
 export function Application() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
   useEffect(() => map.setTarget(mapRef.current), []);
 
-  const layers = useMemo(() => [osmLayer], []);
+  const layers = useMemo(() => [vectorTileLayer], []);
   useEffect(() => map.setLayers(layers), []);
 
   return <div ref={mapRef} />;
