719d7432cfc8893f5d8a403557c2d4b5a49573a0
Johannes Brodwall <johannes@brodwall.com>
2024-05-05 20:38:23 +0200

    npm install -D husky typescript prettier
    * npx tsc --init --react jsx
    * npx prettier --write .
    * npx husky init
    * npm pkg set scripts.test="tsc --noEmit && prettier --check ."

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<h1>OpenLayers React demo</h1>)
+const root = createRoot(document.getElementById("root")!);
+root.render(<h1>OpenLayers React demo</h1>);
