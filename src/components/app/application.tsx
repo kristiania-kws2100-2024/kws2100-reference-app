95e8871e19cb83ed3d8e16882dc2d540bae9f5a0
Johannes Brodwall <johannes@brodwall.com>
2024-05-05 21:19:03 +0200

    Hover and style vector tiles

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { Map, View } from "ol";
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
+import { Feature, Map, View } from "ol";
 import { useGeographic } from "ol/proj";
-import { MutableRefObject, useEffect, useMemo, useRef } from "react";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 
@@ -9,10 +9,13 @@ import "ol/ol.css";
 import VectorTileLayer from "ol/layer/VectorTile";
 import VectorTileSource from "ol/source/VectorTile";
 import { MVT } from "ol/format";
+import { Fill, Stroke, Style } from "ol/style";
+import { unByKey } from "ol/Observable";
+import { FeatureLike } from "ol/Feature";
 
 useGeographic();
 const map = new Map({
-  view: new View({ center: [10, 60], zoom: 8 }),
+  view: new View({ center: [10, 60], zoom: 5 }),
 });
 
 const osmLayer = new TileLayer({ source: new OSM() });
@@ -27,12 +30,54 @@ const vectorTileLayer = new VectorTileLayer({
   }),
 });
 
+const nonActiveStyle = new Style({
+  stroke: new Stroke({
+    color: "red",
+    width: 2,
+  }),
+  fill: new Fill({
+    color: "white",
+  }),
+});
+const activeStyle = new Style({
+  stroke: new Stroke({
+    color: "red",
+    width: 2,
+  }),
+  fill: new Fill({
+    color: "pink",
+  }),
+});
+
 export function Application() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
   useEffect(() => map.setTarget(mapRef.current), []);
 
   const layers = useMemo(() => [vectorTileLayer], []);
   useEffect(() => map.setLayers(layers), []);
+
+  const [activeFeature, setActiveFeature] = useState<FeatureLike>();
+
+  useEffect(() => {
+    const key = map.on("pointermove", (e) => {
+      const features = map.getFeaturesAtPixel(e.pixel);
+      setActiveFeature(features.length === 1 ? features[0] : undefined);
+    });
+    return () => unByKey(key);
+  }, []);
+
+  useEffect(() => {
+    console.log(activeFeature?.getProperties());
+    const admin = activeFeature?.getProperties().admin;
+    function styleFunction(f: FeatureLike) {
+      if (f.getProperties().admin === admin) {
+        return activeStyle;
+      } else {
+        return nonActiveStyle;
+      }
+    }
+    vectorTileLayer.setStyle(styleFunction);
+  }, [activeFeature]);
 
   return <div ref={mapRef} />;
 }
